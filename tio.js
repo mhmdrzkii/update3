32856b9121a8591ec209a11259ddc1c879fa6ccf
MUHAMAD RIZKY <64081774+mhmdrzkii@users.noreply.github.com>
2023-04-07 21:13:52 +0700

    Update tio.js

--- tio.js
+++ tio.js
@@ -353,7 +353,7 @@ tio.readMessages([m.key])
       tio.sendMessage(m.chat, {
         react: {
           text: "🗿",
-          key: m.key,
+          key: m.key, 
         },
       })
     }
@@ -362,7 +362,7 @@ tio.readMessages([m.key])
     if (m.message && m.isGroup) {
       const sewa = JSON.parse(fs.readFileSync("./database/sewa.json"))
       if (!checkSewaGrup(m.chat, sewa))
-      if (!isCreator) throw mess.owner {
+      if (!isCreator) throw mess.owner
         await tio.sendMessage(m.chat, {
           text: `*Maaf, Grup Anda Tidak Terdaftar Dalam Database Kami*`,
         })
